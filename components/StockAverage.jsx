1f90491f6542e3a3241c60428eaf281c652fb813
biren99 <birendra.kumar.consultant@heart.org>
2024-05-01 15:42:34 +0530

    style to stock average

--- components/StockAverage.jsx
+++ components/StockAverage.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import styles from '../styles/stockaverage.module.css'
+
 function StockAverage() {
   const [stockPrice1, setStockPrice1] = useState('');
   const [noOfStock1, setNoOfStock1] = useState('');
@@ -32,35 +34,41 @@ function StockAverage() {
 
   return (
     <div className='flex-col items-center m-3 px-3 '>
+      <form>
       <h1 className='text-center'>Investment Calculator</h1>
-      <h2>For Initial Investment:</h2>
-      <div>
-        <label>
+      
+      <h2>For Initial Investment: Stock at which you have already purchased</h2>
+      
+      <div className="my-2">
+        <label >
             Stock Price:
-            <input type="number" value={stockPrice1} onChange={(e) => setStockPrice1(e.target.value)} />
+            <input type="number" className={`${styles.input_text}`} value={stockPrice1} required onChange={(e) => setStockPrice1(e.target.value)} />
         </label>
         <label>
             No of Stock:
-            <input onKeyUp={initialInvestedAmount} type="number" value={noOfStock1} onChange={(e) => setNoOfStock1(e.target.value)} />
+            <input onKeyUp={initialInvestedAmount} className={`${styles.input_text}`} required type="number" value={noOfStock1} onChange={(e) => setNoOfStock1(e.target.value)} />
         </label>
         {initialInvestment && <div>Initial Invested Amount: {initialInvestment}</div>}
       </div>
-      <div>
+      <hr/>
+      <div className="my-2">
         <h2>For New Investment:</h2>
         <label>
             Stock Price:
-            <input  type="number" value={stockPrice2} onChange={(e) => setStockPrice2(e.target.value)} />
+            <input  type="number" value={stockPrice2} className={`${styles.input_text}`} required onChange={(e) => setStockPrice2(e.target.value)} />
         </label>
         <label>
             No of Stock:
-            <input onKeyUp={NextInvestedAmount} type="number" value={noOfStock2} onChange={(e) => setNoOfStock2(e.target.value)} />
+            <input onKeyUp={NextInvestedAmount} type="number" className={`${styles.input_text}`} required value={noOfStock2} onChange={(e) => setNoOfStock2(e.target.value)} />
         </label><br />
         {newInvestment && <div>New Invested Amount: {newInvestment}</div>}
+        <hr/>
       </div>
-          <button onClick={calculateInvestments}>Calculate</button>
+          <button onSubit={calculateInvestments} className={`${styles.button_calculate}`}>Calculate</button>
           {finalNoOfStock && <div>No of Stock: {finalNoOfStock}</div>}
             {totalInvestment && <div>Total Investment: {totalInvestment}</div>}
             {newAvg && <div>New Avg: {newAvg}</div>}
+    </form>
     </div>
   );
 }
